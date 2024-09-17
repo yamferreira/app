8d6b25b02857fb49eef16dbd5ebd7a2a8b9409a6
yamferreira <yamferreira6@gmail.com>
2024-09-16 23:27:36 -0300

    menu da aplicação

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-const start = () => {
+const { select } = require ('@inquirer/prompts')
+
+const start = async () => {
     while(true){
-        let opcao = "sair"
+        
+        const opcao = await select ({
+            message: "Menu >",
+            choices: [
+                {
+                    name: "Cadastrar meta",
+                    value: "Cadastrar"
+                },
+                {
+                    name: "Listar metas",
+                    value: "listar"
+
+                },
+                {
+                    name: "Sair",
+                    value: "sair"
+                }
+            ]
+        })
+
         switch(opcao){
             case "cadastrar":
                 console.log("Vamos cadastrar")
@@ -9,6 +30,7 @@ const start = () => {
                 console.log("Vamos listar")
                 break
             case "sair":
+                console.log("Até a proxima!")
                 return
         }
 
