bbc1f692514301f24700751a326da0438f42b45e
yamferreira <yamferreira6@gmail.com>
2024-09-17 23:26:30 -0300

    achando o erro do terminal

--- index.js
+++ index.js
@@ -1,36 +1,38 @@
-const { select, input } = require ('@inquirer/prompts')
+const { select, input } = require('@inquirer/prompts')
 
 let meta = {
-    value: 'Tomar 3 Litros de água por dia',
+    value: 'Tomar 3l de água por dia',
     checked: false,
 }
+
 let metas = [ meta ]
 
 const cadastrarMeta = async () => {
-    const meta = await input ({ message: "Digite a meta:"})
+    const meta = await input ({message: "Digite a meta:"})
 
-    if(meta.length == 0) {
-        console.log('A meta não pode ser vazia')
-        return
-
+    if(meta.length == 0){
+        console.log('A meta não pode ser vazia.')
+        return 
     }
+
     metas.push(
-        {value: meta, checked: false}
+        { value: meta, checked: false}
     )
 }
 
 const start = async () => {
-    while(true){
+
+    while (true){
         
         const opcao = await select ({
             message: "Menu >",
             choices: [
                 {
                     name: "Cadastrar meta",
-                    value: "Cadastrar"
+                    value: "cadastrar"
                 },
                 {
-                    name: "Listar metas",
+                    name: "Listar meta",
                     value: "listar"
                 },
                 {
@@ -40,21 +42,21 @@ const start = async () => {
             ]
         })
 
-        switch(opcao){
+
+        switch(opcao) {
             case "cadastrar":
                 await cadastrarMeta()
-                console.log("metas")
+                console.log(metas)
                 break
-            case "Listar":
-                console.log("Vamos listar")
+            case "listar":
+                console.log("vamos listar")
                 break
             case "sair":
-                console.log("Até a proxima!")
+                console.log("Até a proxima!!!")
                 return
         }
+    }     
+}
 
-    }
-   
-    }
 
-start() 
+start()
